c477879a2ae1e9ffcf6c28cda77310f30556db95
lealalves <leealalves@hotmail.com>
2023-04-02 19:52:28 -0300

    conexao login

--- telas/login/index.js
+++ telas/login/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, View, Text, TextInput, Image, TouchableHighlight  } from 'react-native';
+import {React, useState} from 'react';
+import { StyleSheet, View, Text, TextInput, Image, TouchableHighlight, Alert  } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons'
 import Cadastro from '../cadastro';
@@ -7,10 +7,48 @@ import Home from '../home';
 import { SafeAreaView } from 'react-native';
 import { getBottomSpace } from 'react-native-iphone-x-helper';
 import { RFValue } from 'react-native-responsive-fontsize';
+import axios from 'axios';
+
+
 
 export default function Login(){
+  const mensagemDadosIncorretos = (message) => 
+
+  Alert.alert(
+    "Erro",
+    message,
+    [
+      {text: "OK", onPress: () => navigation.navigate('Login')}
+    ],
+    {cancelable: true}
+    );
 
 		const navigation = useNavigation();
+    const url = 'http://192.168.0.107:5000/login';
+    const [senha, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+
+    async function login() {
+      try {
+        const data = {email, senha}
+
+        const req = await fetch(url, {
+          method: 'POST',
+          headers: { 'Content-type': 'application/json' },
+          body: JSON.stringify(data)
+        })
+        const res = await req.json()
+
+        if(req.status !== 200) {
+          return mensagemDadosIncorretos(res.message);
+        }
+
+        console.log('Logou', res);
+      } catch (error) {
+        console.log('DEU ERRO', error);
+      }
+    }
+
 				return(
 						<View style={styles.container}>    
 										<View style={[styles.logoDiv]}>
@@ -22,17 +60,17 @@ export default function Login(){
 												<View style={[styles.InputIcon]}>
 													<Ionicons size={20} name="mail-outline"></Ionicons>
 												</View>
-												<TextInput style={styles.Input} placeholder="Insira seu e-mail"></TextInput>
+												<TextInput onChangeText={(email) => setEmail(email)} style={styles.Input} placeholder="Insira seu e-mail"></TextInput>
 											</View>       
 
 											<View style={[styles.DivInput]}>
 												<View style={[styles.InputIcon]}>
 													<Ionicons size={20} name="lock-closed-outline"></Ionicons>
 												</View>
-												<TextInput style={styles.Input} placeholder="Insira sua senha"></TextInput>
+												<TextInput onChangeText={(password) => setPassword(password)} style={styles.Input} placeholder="Insira sua senha"></TextInput>
 											</View>
 
-											<TouchableHighlight title="Login..." onPress={() => navigation.navigate(Home)} style={[styles.botao, styles.botaologin]}>
+											<TouchableHighlight title="Login..." onPress={login} style={[styles.botao, styles.botaologin]}>
 													<Text style={styles.TextoBotao}>ACESSAR</Text>
 											</TouchableHighlight>
 
