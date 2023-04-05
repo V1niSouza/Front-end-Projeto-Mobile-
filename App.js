b7ae56b1b23204508c86dbc572155336e80da6c9
lealalves <leealalves@hotmail.com>
2023-04-05 14:24:31 -0300

    organização da ordem das telas

--- App.js
+++ App.js
@@ -19,130 +19,126 @@ export default function App() {
 <NavigationContainer>
   <Stack.Navigator>
 
-  <Stack.Screen
-          name="Cadastro"
-          component={Cadastro}
-          options={{
-            title: 'Tela Cadastro',
-            headerStyle:{
-              backgroundColor: '#12ed',
-            },   
-            headerShown: false         
-          }}>
-       </Stack.Screen>
+    <Stack.Screen
+      name="Login"
+      component={Login}
+      options={{
+      title: 'Tela Login',
+      headerStyle:{
+        backgroundColor: '#12ed',
+      },   
+      headerShown: false         
+      }}>
+    </Stack.Screen>
 
-          <Stack.Screen
-                    name="Login"
-                    component={Login}
-                    options={{
-                    title: 'Tela Login',
-                    headerStyle:{
-                      backgroundColor: '#12ed',
-                    },   
-                    headerShown: false         
-                    }}>
-                </Stack.Screen>
-
-
+    <Stack.Screen
+      name="Cadastro"
+      component={Cadastro}
+      options={{
+        title: 'Tela Cadastro',
+        headerStyle:{
+          backgroundColor: '#12ed',
+        },   
+        headerShown: false         
+      }}>
+    </Stack.Screen>
        
-      <Stack.Screen
-          name="Cadastro2"
-          component={Cadastro2}
-          options={{
-            title: 'Tela Cadastro',
-            headerStyle:{
-              backgroundColor: '#12ed',
-            },   
-            headerShown: false         
-          }}>
-       </Stack.Screen>
+    <Stack.Screen
+      name="Cadastro2"
+      component={Cadastro2}
+      options={{
+        title: 'Tela Cadastro',
+        headerStyle:{
+          backgroundColor: '#12ed',
+        },   
+        headerShown: false         
+      }}>
+    </Stack.Screen>       
 
+    <Stack.Screen
+      name="Home"
+      component={Home}
+      options={{
+        title: 'Tela Home',
+        headerStyle:{
+          backgroundColor: '#12ed',
+        },   
+        headerShown: true         
+      }}>
+    </Stack.Screen>
 
-       
+    <Stack.Screen
+      name="Carrinho"
+      component={Carrinho}
+      options={{
+        title: 'Tela Carrinho',
+        headerStyle:{
+          backgroundColor: '#12ed',
+        },   
+        headerShown: false         
+      }}>
+    </Stack.Screen>
 
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{
-            title: 'Tela Home',
-            headerStyle:{
-              backgroundColor: '#12ed',
-            },   
-            headerShown: true         
-          }}>
-        </Stack.Screen>
+    <Stack.Screen
+      name="Catego"
+      component={Catego}
+      options={{
+        title: 'Tela Categoria',
+        headerStyle:{
+          backgroundColor: '#12ed',
+        },   
+        headerShown: false         
+      }}>
+    </Stack.Screen>
 
-        <Stack.Screen
-          name="Carrinho"
-          component={Carrinho}
-          options={{
-            title: 'Tela Carrinho',
-            headerStyle:{
-              backgroundColor: '#12ed',
-            },   
-            headerShown: false         
-          }}>
-       </Stack.Screen>
+    <Stack.Screen
+      name="Compra"
+      component={Compra}
+      options={{
+        title: 'Tela de Compra',
+        headerStyle:{
+          backgroundColor: '#12ed',
+        },   
+        headerShown: false         
+      }}>
+    </Stack.Screen>
 
-        <Stack.Screen
-          name="Catego"
-          component={Catego}
-          options={{
-            title: 'Tela Categoria',
-            headerStyle:{
-              backgroundColor: '#12ed',
-            },   
-            headerShown: false         
-          }}>
-        </Stack.Screen>
+    <Stack.Screen
+      name="Listaprodu"
+      component={Listaprodu}
+      options={{
+        title: 'Tela Lista de Produto',
+        headerStyle:{
+          backgroundColor: '#12ed',
+        },   
+        headerShown: false         
+      }}>
+    </Stack.Screen>
 
-        <Stack.Screen
-          name="Compra"
-          component={Compra}
-          options={{
-            title: 'Tela de Compra',
-            headerStyle:{
-              backgroundColor: '#12ed',
-            },   
-            headerShown: false         
-          }}>
-        </Stack.Screen>
+    <Stack.Screen
+      name="Produt"
+      component={Produt}
+      options={{
+        title: 'Tela Produto',
+        headerStyle:{
+          backgroundColor: '#12ed',
+        },   
+        headerShown: false         
+      }}>
+    </Stack.Screen>
 
-        <Stack.Screen
-          name="Listaprodu"
-          component={Listaprodu}
-          options={{
-            title: 'Tela Lista de Produto',
-            headerStyle:{
-              backgroundColor: '#12ed',
-            },   
-            headerShown: false         
-          }}>
-        </Stack.Screen>
+    <Stack.Screen
+      name="Avalia"
+      component={Avalia}
+      options={{
+        title: 'Tela Avaliação',
+        headerStyle:{
+          backgroundColor: '#12ed',
+        },   
+        headerShown: false         
+      }}>
+    </Stack.Screen>
 
-        <Stack.Screen
-          name="Produt"
-          component={Produt}
-          options={{
-            title: 'Tela Produto',
-            headerStyle:{
-              backgroundColor: '#12ed',
-            },   
-            headerShown: false         
-          }}>
-        </Stack.Screen>
-
-        <Stack.Screen
-          name="Avalia"
-          component={Avalia}
-          options={{
-            title: 'Tela Avaliação',
-            headerStyle:{
-              backgroundColor: '#12ed',
-            },   
-            headerShown: false         
-          }}>
-        </Stack.Screen>
   </Stack.Navigator>
 </NavigationContainer>
   );
